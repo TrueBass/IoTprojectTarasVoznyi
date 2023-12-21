c3133a9b876b7db12cb3513e527ce91b94e9ea61
TrueBass <ttrrbb81@gmail.com>
2023-12-21 22:23:59 +0100

    OPC UA connection, machine data reading

---
+++ main.py
@@ -0,0 +1,34 @@
+from machine_h import Machine, asyncio
+from asyncua import Client
+from azure.iot.device import IoTHubDeviceClient
+import json
+
+# prod = await devices_list[0].get_child("ProductionRate")
+# await client.set_values([client.get_node(prod)], [ua.DataValue(ua.Variant(int(10), ua.VariantType.Int32))])
+
+async def send_messages_async(device, machine):
+    message = machine
+    await device.send_message(message)
+
+async def main():
+    client = Client("opc.tcp://localhost:4840/")    #  OPC UA server connection
+    await client.connect()                          #
+
+    devices_list = await client.get_objects_node().get_children()   #  get all devices
+    devices_list = devices_list[1:]                                 #
+
+    for device in devices_list:
+        machine = Machine(client, device)
+        await machine.insert_data()
+        print(machine)
+
+    connection_string = "HostName=iot-hub-taras-ul.azure-devices.net;DeviceId=testDevice;SharedAccessKey=K54t01Bf5sfFuVFBHiEz3GPNUOZ6AbfDuAIoTL9f1p4="
+
+    iot_device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+
+    iot_device_client.connect()
+
+    await send_messages_async(iot_device_client,devices_list[0])
+
+if __name__ == '__main__':
+    asyncio.run(main())
