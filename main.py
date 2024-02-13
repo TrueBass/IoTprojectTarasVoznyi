55ebe8fb894cd3892347ce9e41cf442f54c6a332
TrueBass <ttrrbb81@gmail.com>
2024-02-13 14:33:44 +0100

    sending data to iot. sending errors if occur. keyboard interruption stops the prog. reported device twin func added.

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def send_messages_device(device: IoTHubDeviceClient, machine: Machine):
     device.send_message(message)
 
 
+async def reported_device_twin(client, machine):
+    reported_properties = {machine.get_name(): {"ProductionRate": machine.production_rate, "Errors": machine.device_error}}
+    client.patch_twin_reported_properties(reported_properties)
+
+
 async def main():
     #   OPC UA server connection
     client = Client("opc.tcp://localhost:4840/")
@@ -24,40 +29,40 @@ async def main():
     iot_device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
     iot_device_client.connect()
 
-    # num_of_messages = 10
     machines_list = []
     old_errors = [[0,0,0,0],[0,0,0,0]]
 
-    while True:
-        devices_list = await client.get_objects_node().get_children()
-        devices_list = devices_list[1:]
+    try:
+        while True:
+            # receiving data from server
+            devices_list = await client.get_objects_node().get_children()
+            devices_list = devices_list[1:]
 
-        for device in devices_list:
-            machine = Machine(client, device)
-            await machine.insert_data()
-            machines_list.append(machine)
+            for device in devices_list:
+                machine = Machine(client, device)
+                await machine.insert_data()
+                machines_list.append(machine)
 
-        for i in range(len(machines_list)):
-            if machines_list[i].get_errors() != [0,0,0,0] and machines_list[i].get_errors() != old_errors[i]:
-                send_messages_device(iot_device_client, machines_list[i])
-                old_errors[i] = machines_list[i].get_errors()
-            if machines_list[i].get_errors() == [0,0,0,0]:
-                old_errors[i] = machines_list[i].get_errors()
+            # sending data to IoT
+            for machine in machines_list:
+                send_messages_device(iot_device_client, machine)
 
-        # if all(machine.get_errors() == [0,0,0,0] for machine in machines_list):
-        #     old_errors.clear()
-        # print(machines_list)
-        #
-        # machines_list = list(filter(lambda m: m.get_errors() != [0, 0, 0, 0], machines_list))
-        #
-        # if len(machines_list) != 0:
-        #     for machine in machines_list:
-        #         if machine.get_errors() not in old_errors:
-        #             old_errors.append(machine.get_errors())
-        #             send_messages_device(iot_device_client, machine)
+            # sending
+            for i in range(len(machines_list)):
+                if machines_list[i].get_errors() != [0,0,0,0] and machines_list[i].get_errors() != old_errors[i]:
+                    machines_list[i].device_error_state = True
+                    send_messages_device(iot_device_client, machines_list[i])
+                    old_errors[i] = machines_list[i].get_errors()
+                if machines_list[i].get_errors() == [0,0,0,0]:
+                    old_errors[i] = machines_list[i].get_errors()
 
-        machines_list.clear()
-        time.sleep(1)
+            machines_list.clear()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+
+    await client.disconnect()
+    iot_device_client.disconnect()
 
 if __name__ == '__main__':
     asyncio.run(main())
